5de2eb2cd5cc5effca0e1f816478821b059452be
Olaonipekun Favour <73252872+flavoury@users.noreply.github.com>
2022-03-18 16:51:12 +0100

    Add files via upload
    Creating the index.html, subject.js, and adding the pictures

---
+++ Vuejs_Individual/subjects.js
@@ -0,0 +1,94 @@
+let subjects = [
+    {
+        "id":101,
+        "image":"img2/math-removebg-preview.png",
+        "subjectname":"Mathematics",
+        "location":"China",
+        "price":400,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":102,
+        "image":"img2/eng-removebg-preview.png",
+        "subjectname":"English",
+        "location":"United Kingdom",
+        "price":200,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":103,
+        "image":"img2/bio.png",
+        "subjectname":"Biology",
+        "location":"Korea",
+        "price":700,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":104,
+        "image":"img2/chem.png",
+        "subjectname":"Chemistry",
+        "location":"Saudi Arabia",
+        "price":300,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":105,
+        "image":"img2/mus-removebg-preview.png",
+        "subjectname":"Music",
+        "location":"Dubia",
+        "price":1200,
+        "numberofspaces": 5,
+       
+    },
+    {
+        "id":106,
+        "image":"img2/agric.png",
+        "subjectname":"Agriculture",
+        "location":"Russia",
+        "price":8100,
+        "numberofspaces": 5,
+        "rating": 2
+    },
+    {
+        "id":107,
+        "image":"img2/comp-removebg-preview (3).png",
+        "subjectname":"Computer",
+        "location":"Ukraine",
+        "price":900,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":108,
+        "image":"img2/art-removebg-preview.png",
+        "subjectname":"Fine Arts",
+        "location":"Malta",
+        "price":1000,
+        "numberofspaces": 5,
+       
+    },
+    {
+        "id":109,
+        "image":"img2/phy-removebg-preview (1).png",
+        "subjectname":"Physics",
+        "location":"Kenya",
+        "price":1200,
+        "numberofspaces": 5,
+        
+    },
+    {
+        "id":110,
+        "image":"img2/his-removebg-preview (1).png",
+        "subjectname":"History",
+        "location":"Nigeria",
+        "price":1400,
+        "numberofspaces": 5,
+        
+    }
+
+   
+  ]
